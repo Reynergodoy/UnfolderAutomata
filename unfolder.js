e4a93eecd6a9559685c9b01b44ede25a25ef1ae6
Rey <reyneraccel@gmail.com>
2019-01-26 17:40:22 -0200

    Create unfolder.js

---
+++ unfolder.js
@@ -0,0 +1,13 @@
+class Unfolder {
+    constructor (anchor, pos, dimensions, colors) {
+        this.anchor = anchor;
+        this.pos = pos;
+        this.dimensions = dimensions;
+        this.colors = colors;
+        this.board = [];
+    }
+    
+    step () {
+    }
+    
+}
